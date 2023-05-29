f0b12a642de94c1adc36a1eee80ea2e6f78f8f16
Mani424242 <123720071+Mani424242@users.noreply.github.com>
2023-05-29 20:07:17 +0530

    Update app.js

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world 1234',
+                message: 'hello world 12344',
                 // location: ret.data.trim()
             })
         }
